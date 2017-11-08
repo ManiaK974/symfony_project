0b6edfd6fc7a6cc7675a39f76b81ce0490d2a54a
ManiaK974 <32074090+ManiaK974@users.noreply.github.com>
2017-11-08 13:58:29 +0100

    Suppression Fonction inutile

--- Controller/VspController.php
+++ Controller/VspController.php
@@ -65,23 +65,4 @@ class VspController extends Controller {
         return $this->render('widget/last_list_vsp.html.twig', array('list' => $listLastAdverts));
     }
 
-    /**
-     * @Route("/add_vsp", name="add_vsp")
-     * @Method({ "GET", "POST" })
-     */
-    public function addAction(Request $request) {
-        $advert = new Vsp();
-        $form = $this->createForm(VspType::class, $advert);
-        if ($form->handleRequest($request)->isValid()) {
-
-            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
-
-            return $this->redirect($this->generateUrl('homepage'));
-        }
-
-        return $this->render('widget/search_widget.html.twig', array(
-                    'form' => $form->createView()
-        ));
-    }
-
 }
